a46489994d626fbebd0d04c39b2b296b0c9c1ce1
codermadeen001 <syeundainnocent@gmail.com>
2025-06-18 09:48:34 +0300

    update all

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -32,11 +32,26 @@ class ServiceController extends Controller
         )
     );
 }
-    */
+    
 
     public function __construct()
 {
     $this->cloudinary = new Cloudinary();
+}*/
+
+
+public function __construct()
+{
+    $this->cloudinary = new Cloudinary([
+        'cloud' => [
+            'cloud_name' => config('cloudinary.cloud_name'),
+            'api_key' => config('cloudinary.api_key'),
+            'api_secret' => config('cloudinary.api_secret'),
+        ],
+        'url' => [
+            'secure' => true
+        ]
+    ]);
 }
 
     public function index()
